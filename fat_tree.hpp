45a722c4c11ed439435503d34c84d6eba64cc7a6
Dmitry Levkin <mit.levkin@vk.com>
2026-04-14 08:17:36 +0300

    create minimum

--- fat_tree.hpp
+++ fat_tree.hpp
@@ -3,28 +3,48 @@
 
 #include <cstddef>
 
+namespace stuff {
+
 template <class T, size_t K>
 struct BTree {
     T val[K];
-    BTree<T, K> children[K + 1];
+    BTree<T, K>* children[K + 1];
     BTree<T, K>* parent;
 };
 
 template <class T, size_t K>
 struct BTreeIt {
-    size_t s;
+    BTreeIt(size_t s, BTree<T, K>* current)
+        : s(s)
+        , current(current)
+    {
+    }
     BTree<T, K>* current;
+    size_t s;
 };
+
+template <class T, size_t K>
+BTreeIt<T, K>* minimum(BTree<T, K> it)
+{
+    if (!it) {
+        return { nullptr, 0 };
+    }
+    while (it.children[0]) {
+        it = it.children[0];
+    }
+    return { it, 0 };
+}
 
 template <class T, size_t K>
 T value(BTreeIt<T, K> it);
 template <class T, size_t K>
-BTree<T, K> next(BTree<T, K> it);
+BTreeIt<T, K>* next(BTree<T, K> it);
 template <class T, size_t K>
-BTree<T, K> prev(BTree<T, K> it);
+BTreeIt<T, K>* prev(BTree<T, K> it);
 template <class T, size_t K>
 bool hasNext(BTree<T, K> it);
 template <class T, size_t K>
 bool hasPrev(BTree<T, K> it);
 
+}
 #endif
